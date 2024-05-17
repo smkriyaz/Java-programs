fee581d29d2d0dd851b8b03fbb1d869b1ca2c71e
ABC <smkriyazoutlook@gmail.com>
2024-05-17 12:33:42 +0530

    swap numbers
    basic programs

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap {
+  public static void main(String[] args) {
+    int a=10;
+    int b=30;
+    int temp;
+
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("a="+a);
+    System.out.println("b="+b);
+  }
+}
