75a7253a1982a2960e9347eac3b155976ddbcb57
ABC <smkriyazoutlook@gmail.com>
2024-05-17 12:51:44 +0530

    swapwithouttemp
    basic programs

---
+++ swapwithout.java
@@ -0,0 +1,14 @@
+public class swapwithout {
+  public static void main(String[] args){
+    int a=35;
+    int b=15;
+
+    a=a-b; //20
+    b=a+b;  //35
+   a=b-a;//15
+   System.out.println(a);
+   System.out.println(b);
+
+
+  }
+}
